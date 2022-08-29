e48a8a425bb4d1114b74266e6c464c1217d8281e
sajjad <sajjadmahmodi051@gmail.com>
2022-08-29 20:53:18 +0430

    assignment2

---
+++ as2-1.py
@@ -0,0 +1,12 @@
+a=float(input('enter your nomre?'))
+b=float(input('enter your nomre?'))
+c=float(input('enter your nomre?'))
+d=float(input('enter your nomre?'))
+e=float(input('enter your nomre?'))
+m=(a+b+c+d+e)/5
+if (17<=m):
+    print('excellent')
+elif(17>m>12):
+    print('normal')
+elif(12>m):
+    print('mashrot')
---
+++ as2-3.py
@@ -0,0 +1,8 @@
+a=float(input('enter add one?'))
+b=float(input('enter add two'))
+c=float(input('enter add three'))
+d=float(input('enter add four'))
+e=float(input('enter add five'))
+values=[a,b,c,d,e]
+values.sort(reverse=True)
+print(values)
